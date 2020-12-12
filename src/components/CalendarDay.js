865caa977ba013035c4d3f806238c4db6c3c17ae
RudyPark3091 <grayblack313@gmail.com>
2020-12-12 14:16:30 +0900

    Add monthly view context

--- src/components/CalendarDay.js
+++ src/components/CalendarDay.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const CalendarDay = (props) => {
-	const Day = styled.div`
-	padding: 10px;
-	background-color: ${props.off ? "#d5d5d5" : "white"};
-	color: #959595;
-	font-size: 20px;
-	font-weight: 300;
+const Day = styled.div`
+padding: 10px;
+background-color: ${props => (
+	props.sunday ? "#ffeeee" :
+		props.saturday ? "#eeeeff" : "#ffffff"
+)};
+color: ${props => (
+	props.sunday ? "#ff0000" :
+		props.saturday ? "#0000ff" : "#959595"
+)};
+font-size: 20px;
+font-weight: 300;
 
-	&:hover {
-		background-color: #e5e5e5;
-	}
-	`;
+&:hover {
+	background-color: #e5e5e5;
+}
+`;
+
+const CalendarDay = (props) => {
 
 	return (
 		<>
-			<Day>{props.text}</Day>
+			<Day sunday={props.sunday} saturday={props.saturday}>{props.text}</Day>
 		</>
 	)
 }
